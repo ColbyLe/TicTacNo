7e619f2a6b99100d8aefa17fcb7fe41b22502c83
ColbyLe <colbyle97@gmail.com>
2019-05-10 23:25:35 -0700

    Initial commit

--- src/TicTacNo.java
+++ src/TicTacNo.java
@@ -1,8 +1,5 @@
 // Colby Le
-import java.io.InputStreamReader;
-import java.io.BufferedReader;
-import java.io.PrintWriter;
-import java.io.IOException;
+import java.io.*;
 import java.net.InetAddress;
 import java.net.Socket;
 
@@ -12,40 +9,101 @@ public class TicTacNo {
     static InetAddress addr;
     static int maxDepth = 9;
     public static void main(String[] args) throws IOException {
+        // instantiate and populate board matrix
         board = new char[3][3];
+        for(int i=0; i<3; i++) {
+            for(int j=0; j<3; j++) {
+                board[i][j] = '-';
+            }
+        }
+
+
         try {
             s1 = new Socket("www.dagertech.net", 3800);
             addr = s1.getInetAddress();
-
             System.out.println("Connecting to " + addr + ":3800");
 
-            InputStreamReader isr = new InputStreamReader(s1.getInputStream());
-            BufferedReader br = new BufferedReader(isr);
+            BufferedReader inFromServer = new BufferedReader(new InputStreamReader(s1.getInputStream(), "UTF-8"));
             PrintWriter pw1 = new PrintWriter(s1.getOutputStream(), true);
-            String serverOut = "";
+            DataOutputStream outToServer = new DataOutputStream(s1.getOutputStream());
+            String serverOut;
+
+            System.out.print("AI-COM/OTTO\nvs\n");
+            while((serverOut = inFromServer.readLine()) != null) {
+                System.out.println(serverOut + "\n");
+            }
+
+            outToServer.writeUTF("X\n");
+            //pw1.write("X\n");
+            serverOut = inFromServer.readLine();
+            System.out.println(serverOut);
 
             do {
-                serverOut = "";
-                //pw1.println("version\n");
-                while((serverOut = br.readLine()) != null) {
-                    System.out.println(serverOut);
-                }
-                pw1.print('x');
-            } while(!serverOut.contains("Wins!"));
+                // get client move
+                makeMove();
+                // print current state of board
+                System.out.println("Client Move: ");
+                printBoard();
 
+                // pass move to server as 9Board
+                outToServer.writeUTF(matrixToNBoard());
+                serverOut = inFromServer.readLine();
+                if(serverOut.length() == 9) {
+                    NBoardToMatrix(serverOut);
+                    System.out.println("Server Move: ");
+                    printBoard();
+                }
+                else {
+                    while((serverOut) != null) {
+                        System.out.println(serverOut);
+                    }
+                }
+                //pw1.println("version\n");
+
+            } while(!serverOut.contains("Wins!"));
             s1.close();
         }
+
         catch (Exception e) {
             System.out.println(e.getMessage());
         }
     }
 
-    static int matrixToNBoard() {
-
-        return 0;
+    static String matrixToNBoard() {
+        // instantiate StringBuilder sb
+        StringBuilder sb = new StringBuilder();
+        // iterate through matrix
+        for(int i=0; i<3; i++) {
+            for(int j=0; j<3; j++) {
+                // if an 'X' or 'O' is encountered, write to sb
+                if(board[i][j] == 'X') sb.append('X');
+                else if(board[i][j] == 'O') sb.append('O');
+                // if any other value is encountered, convert matrix indices to integer value and write value to sb
+                else {
+                    int wr=0;
+                    switch(i) {
+                        case 0:
+                            wr = i+j+1;
+                            break;
+                        case 1:
+                            wr = i+j+3;
+                            break;
+                        case 2:
+                            wr = i+j+5;
+                            break;
+                    }
+                    sb.append(wr);
+                }
+            }
+        }
+        // return string
+        return sb.toString();
     }
 
     static void NBoardToMatrix(String NBoard) {
+        // iterate through 9Board
+        // if 9Board contains an X or O,
+        // replace value at corresponding position of board matrix with appropriate character
         for(int i=0; i< NBoard.length(); i++) {
             if(NBoard.charAt(i) == 'X') {
                 switch(i) {
@@ -79,11 +137,37 @@ public class TicTacNo {
                 }
 
             }
+
             else if(NBoard.charAt(i) == 'O') {
-
-            }
-            else {
-
+                switch(i) {
+                    case 0:
+                        board[0][0] = 'O';
+                        break;
+                    case 1:
+                        board[0][1] = 'O';
+                        break;
+                    case 2:
+                        board[0][2] = 'O';
+                        break;
+                    case 3:
+                        board[1][0] = 'O';
+                        break;
+                    case 4:
+                        board[1][1] = 'O';
+                        break;
+                    case 5:
+                        board[1][2] = 'O';
+                        break;
+                    case 6:
+                        board[2][0] = 'O';
+                        break;
+                    case 7:
+                        board[2][1] = 'O';
+                        break;
+                    case 8:
+                        board[2][2] = 'O';
+                        break;
+                }
             }
         }
     }
@@ -97,10 +181,6 @@ public class TicTacNo {
             }
             System.out.println();
         }
-    }
-
-    static void getServerMove() {
-
     }
 
     static int evaluate() {
